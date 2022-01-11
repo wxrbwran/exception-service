d31eefb099df9cd418ae2c6b3d0c78b2ec8f09bf
wuxiaoran <wuxiaoran@xinzhili.cn>
2022-01-11 18:30:18 +0800

    加入sentinel

---
+++ exception-transfer/src/main/java/com/xzlcorp/exception/transfer/controller/FlowLimitController.java
@@ -0,0 +1,18 @@
+package com.xzlcorp.exception.transfer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FlowLimitController {
+
+  @GetMapping("/testA")
+  public String testA() {
+    return "-----testA";
+  }
+
+  @GetMapping("/testB")
+  public String testB() {
+    return "-----testB";
+  }
+}
